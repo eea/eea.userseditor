1cb19d8e6e8df95328ebe5471a7161e1db511cbd
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2020-02-12 16:29:43 +0200

    compatibility changes for gitflow integration

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+import os
 from setuptools import setup, find_packages
 
-setup(name='eea.userseditor',
-      version='1.1.39',
+NAME = "eea.userseditor"
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
       include_package_data=True,
       platforms=['OS Independent'],
       zip_safe=False,
-      install_requires=['eea.usersdb>=1.3.40', 'deform', 'phonenumbers', 'six'],
+      install_requires=['eea.usersdb>=1.3.40', 'deform', 'phonenumbers',
+                        'six'],
       )
