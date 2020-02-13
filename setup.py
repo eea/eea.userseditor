f460875f0ee47892053a9fcfe85456603c4f6798
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-02-13 13:29:23 +0200

    Add fields for setup.py release on pypi

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ VERSION = open(os.path.join(*PATH)).read().strip()
 
 setup(name=NAME,
       version=VERSION,
+      description="EEA Users Editor",
+      long_description_content_type="text/x-rst",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open("CHANGELOG.rst").read()
+      ),
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
